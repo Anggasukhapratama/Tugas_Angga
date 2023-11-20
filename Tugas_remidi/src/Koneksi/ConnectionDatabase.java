926b20a44e0276cdce9bbf9a74aa84ac9bb4d327
Anggasukhapratama <118103208+Anggasukhapratama@users.noreply.github.com>
2023-11-20 13:08:40 +0700

    Add files via upload

---
+++ Tugas_remidi/src/Koneksi/ConnectionDatabase.java
@@ -0,0 +1,39 @@
+package Koneksi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import javax.swing.JOptionPane;
+
+public class ConnectionDatabase {
+
+  public static Connection getKoneksi() {
+    try {
+        Properties prop = new Properties();
+        InputStream input = ConnectionDatabase.class.getClassLoader().getResourceAsStream("config.properties");
+
+        if (input == null) {
+            System.out.println("Sorry, unable to find config.properties");
+            return null;
+        }
+
+        prop.load(input);
+
+        String url = prop.getProperty("db.url=jdbc:mysql://localhost:3306/your_users");
+        String username = prop.getProperty("db.username");
+        String password = prop.getProperty("db.password");
+
+        System.out.println("URL: " + url);
+        System.out.println("Username: " + username);
+
+        return DriverManager.getConnection(url, username, password);
+    } catch (IOException | SQLException e) {
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(null, "Database connection error: " + e.getMessage(), "Connection Error", JOptionPane.ERROR_MESSAGE);
+        return null;
+    }
+}
+}
